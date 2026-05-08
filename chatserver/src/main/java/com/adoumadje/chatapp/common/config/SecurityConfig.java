7e39b3a632f7dae3c247385aac9930ce3d70d1ba
adoumadje <adoumadjecaleb@gmail.com>
2026-05-08 15:43:00 +0200

    google oauth started

---
+++ chatserver/src/main/java/com/adoumadje/chatapp/common/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.adoumadje.chatapp.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        http.sessionManagement(sessionConfig ->
+                sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.csrf(csrfConfig -> csrfConfig
+                .ignoringRequestMatchers("/api/v1/users"));
+
+        http.authorizeHttpRequests(requestConfig -> requestConfig
+                .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                .anyRequest().authenticated());
+
+        http.oauth2ResourceServer(rsConfig -> rsConfig
+                .jwt(jwtConfig -> jwtConfig.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+        return http.build();
+    }
+}
